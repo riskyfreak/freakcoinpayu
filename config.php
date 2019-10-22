d9a5aa28ca4eb788d9fbe06a18e52f7d69b59740
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-10-22 18:08:26 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0";
+$token = "2c2b5423165bda3dd237c4c8fa502537ff830ba8";
+$PHPSESSID = "PHPSESSID=6et6kh0c0vn866j2dguu1cp3vp";
+$cfduid = "__cfduid=dc52265672bc0d4d53c4bb3ff26c8834f1571738535; PHPSESSID=6et6kh0c0vn866j2dguu1cp3vp";
